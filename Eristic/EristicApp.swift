a0e2e7bad90162e74e042d4c2767c03e323ce8eb
Fady Eid <feid@apple.com>
2023-12-06 23:36:56 -0800

    Initial Commit

---
+++ Eristic/EristicApp.swift
@@ -0,0 +1,17 @@
+//
+//  EristicApp.swift
+//  Eristic
+//
+//  Created by Fady A Eid on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EristicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
